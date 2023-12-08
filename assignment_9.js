5b3e3aa0420c3b6f40326904a0527d1d1a92fde5
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2023-12-08 08:03:32 -0600

    Update assignment_9.js

--- assignment_9.js
+++ assignment_9.js
@@ -1,28 +1,47 @@
-"use strict";
-class Student{
-    #studentID;
-    #answers = {studentID: "", questionID: "", answer: ""};
+// "use strict";
+// class Student{
+//     #studentID;
+//     #answers = {studentID: "", questionID: "", answer: ""};
 
-    addAnswer(question){
+//     addAnswer(question){
 
+//     }
+// }
+
+// class Question extends Quiz{
+//     #questionID;
+//     #answer = {questionID: "", answer: ""};
+//     checkAnswer(answer){
+//         let ob = new Quiz();
+//         if (this.#answer.answer == answer) return true;
+//     }
+// }
+
+// class Quiz{
+//     #questions = new Map();
+//     #students = [];
+//     get students(){
+//         return this.#students;
+//     }
+//     get questions(){
+//         return this.#questions;
+//     }
+//     scoreStudentBySid(sid){
+
+//     }
+//     getAverageScore(){
+
+//     }
+// }
+function greeting() { 
+    console.log(this);
     }
-}
-
-class Question{
-    #questionID;
-    #answer = {questionID: "", answer: ""};
-    checkAnswer(answer){
-        if (this.#answer == answer) return true;
+let user = { firstName:
+    "John", sayHi() {
+    console.log(this);
     }
-}
-
-class Quiz{
-    #questions = new Map();
-    #students = [];
-    scoreStudentBySid(sid){
-
-    }
-    getAverageScore(){
-        
-    }
-}
+    };  
+    console.log(this); // this is window object
+    greeting(); // greeting() is called by global window object, this is window
+    user.sayHi(); 
+    
